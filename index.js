4f5ee70767d1bcf8519ebed117681a746eb355cd
Aaron Feinberg <aaron.feinberg@wework.com>
2019-01-23 01:57:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,9 +25,8 @@ function makeInt(n) {
 function makeInt(n){
   return parseInt(n, 10);
 }
-}
 function preserveDecimal(n){
-  parseFloat(n)
+  return parseFloat(n)
 }
 function preserveDecimal(n){
   parseInt('nonsense!')
